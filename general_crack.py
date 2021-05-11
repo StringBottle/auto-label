414cbea19b765d19054f258b89cd297ce8bd5cf2
stringbottle <stringbottle.mail@gmail.com>
2021-05-12 00:01:41 +0900

    Add configuration files

---
+++ general_crack.py
@@ -0,0 +1,11 @@
+auto_label_dir = "/home/sss/UOS-SSaS Dropbox/05. Data/06. Auto Labeling"
+
+dataset_name = 'General Crack'
+raw_img_dir_name = '/home/sss/UOS-SSaS Dropbox/05. Data/06. Auto Labeling/General Crack/01. Raw Image'
+rsz_img_dir_name = '02. Resized Image'
+pre_label_dir_name = '03. Pre-labeled Psd Files'
+corrected_label_dir_name = '04. Corrected Psd files' 
+rsz_img_size = 2048
+
+config_file = '/home/sss/UOS-SSaS Dropbox/05. Data/03. Checkpoints/2020.09.02_deeplabv3plus_r101-d8_769x769_40k_concrete_damage_cs/deeplabv3plus_r101-d8_769x769_40k_concrete_damage_cs.py'
+checkpoint_file = '/home/sss/UOS-SSaS Dropbox/05. Data/03. Checkpoints/2020.09.02_deeplabv3plus_r101-d8_769x769_40k_concrete_damage_cs/iter_40000.pth'
---
+++ kcqr_config.py
@@ -0,0 +1,23 @@
+configs = dict(
+    auto_label_dir = "/home/sss/UOS-SSaS Dropbox/05. Data/06. Auto Labeling",
+    dataset_name = 'KCQR', 
+    rsz_img_size = 1024, 
+
+    model_config_file = '/home/sss/UOS-SSaS Dropbox/05. Data/03. Checkpoints/2020.09.02_deeplabv3plus_r101-d8_769x769_40k_concrete_damage_cs/deeplabv3plus_r101-d8_769x769_40k_concrete_damage_cs.py', 
+    model_checkpoint_file = '/home/sss/UOS-SSaS Dropbox/05. Data/03. Checkpoints/2020.09.02_deeplabv3plus_r101-d8_769x769_40k_concrete_damage_cs/iter_40000.pth', 
+
+    num_of_class = 4 , # not include background 
+    erode_size = 2,
+
+    palette = [
+        [255, 0, 0], # crack 
+        [0, 255, 0], # effl 
+        [0, 255, 255], # rebar
+        [255, 255, 0], # spalling
+    ], 
+
+    classes = ['crack', 'efflorescence', 'rebar', 'spalling'], 
+    )
+    
+
+
