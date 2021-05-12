6a01aa253db479a3a71d3b2d81f72f7a4025fb6d
stringbottle <stringbottle.mail@gmail.com>
2021-05-12 15:12:31 +0900

    Modify general crack config name

---
+++ general_crack_config.py
@@ -0,0 +1,20 @@
+configs = dict(
+    auto_label_dir = "/home/sss/UOS-SSaS Dropbox/05. Data/06. Auto Labeling",
+    dataset_name = 'General Crack',
+    rsz_img_size = 2048,
+
+    model_config_file = '/home/sss/UOS-SSaS Dropbox/05. Data/03. Checkpoints/2020.09.02_deeplabv3plus_r101-d8_769x769_40k_concrete_damage_cs/deeplabv3plus_r101-d8_769x769_40k_concrete_damage_cs.py',
+    model_checkpoint_file = '/home/sss/UOS-SSaS Dropbox/05. Data/03. Checkpoints/2020.09.02_deeplabv3plus_r101-d8_769x769_40k_concrete_damage_cs/iter_40000.pth',
+
+    num_of_class = 1 , # not include background 
+    erode_size = 2,
+
+    palette = [
+        [255, 0, 0], # crack 
+    ], 
+
+    classes = ['crack',], 
+    )
+    
+
+
